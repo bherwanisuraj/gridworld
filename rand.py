98f1136c48c8f0f9336012ad814e95d4b0fdac02
bherwanisur <bherwanisur@gmail.com>
2024-01-24 14:59:49 +0530

    training

--- rand.py
+++ rand.py
@@ -1,8 +1,1 @@
-x= 1
-y = 0.1
-z= 1
-
-for i in range(10000):
-    print(f"{x/y}  |  {x/z}")
-    y = y - (y/y**2)
-    z = z - (z/z**2)
+model.fit(np.array(states), np.array(target_batch), epochs=1, verbose=0)
